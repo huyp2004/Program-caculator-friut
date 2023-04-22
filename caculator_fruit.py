380e9c19d38f0546d19db49a6e7e6876037d6925
FIRST_NAME LAST_NAME <quochuyp2004@gmail.com>
2023-04-22 15:43:48 +0700

    Update input user_choose

--- caculator_fruit.py
+++ caculator_fruit.py
@@ -1,56 +1,62 @@
 # Quản lý và gọi các chức năng thực hiện mua hàng, tính toán
 def main():
     while True:
-        fruit = shop()
-        print(caculator_buy(fruit))
+        fruit_shop = shop()
+        print(caculator_buy(fruit_shop))
         if input("Bạn có muốn mua tiếp? Có/Không >").lower() == ["khong", "không", "k", "ko", "no", "n"]:
             exit()
 
 
 # Thông tin của trái cây, giá tiền
 def shop():
-    fruit = dict(
+    fruit_shop = dict(
         Táo=75000,  # Táo nữ hoàng 1Kg/75k
         Bưởi=52000,  # Bưởi da xanh 1Kg/52k
         Lê=66000  # Lê dường 1kg/66k
     )
-    return fruit
+    return fruit_shop
 
 
 # người dùng chọn mua trái cây và thêm vào danh sách mua, sau khi chọn xong sẽ tính tiền
 def user_choose(fruit_shop):
-
+    # Danh sách trái cây
     print(list(fruit_shop))
 
     user = input("Bạn chọn mua gì > ").title()
-    while True:
-        if user not in fruit_shop:
-            user = input("Bạn chọn mua lại > ").title()
-        else:
-            break
-    kg = int(input("Bạn mua bao nhiêu kg > "))
+    # Trường hợp sai
+    while user not in fruit_shop:
+        user = input("Bạn chọn mua lại > ").title()
+    
+    kg = input("Bạn mua bao nhiêu kg > ")
+    # Trường hợp sai
+    while not kg.isdigit():
+        kg = input("Bạn mua bao nhiêu kg > ")
 
-    return user, kg
+    return user, int(kg)
+    
 
-
-# Hàm tính tiền mua trái cây
+# Hàm tính tiền mua trái cây: Thối tiền, tiền thiếu../.
 def caculator_buy(fruit_shop):
-    choose, cost = user_choose(fruit_shop)
+    try:
+        choose, cost = user_choose(fruit_shop)
 
-    sum_money = fruit_shop[choose] * cost
-    print(f"Tổng {choose} có {cost}kg là: {sum_money}")
-
-    user_money = float(input("Khách đưa tiền > "))
-
-    if choose in fruit_shop:
+        # Tổng tiền cần trả
         sum_money = fruit_shop[choose] * cost
+        print(f"Tổng {choose} có {cost}kg là: {sum_money}")
 
-        if user_money < sum_money:
-            return f"Số tiền còn thiếu: {user_money - sum_money} VND"
-        elif user_money == sum_money:
-            return "Số tiền đã đủ"
-        else:
-            return f"Số tiền thối là {user_money - sum_money} VND"
+        # Tiền Khách đưa
+        user_money = float(input("Khách đưa tiền > "))
 
+        if choose in fruit_shop:
+            if user_money < sum_money: # Thiếu tiền
+                return f"Số tiền còn thiếu: {user_money - sum_money} VND"
+            
+            elif user_money == sum_money: # Đủ tiền
+                return "Số tiền đã đủ"
+        
+            else:  # Thối tiền
+                return f"Số tiền thối là {user_money - sum_money} VND"
+    except ValueError:
+        return "sai ở đâu ấy ai biết"
 
 main()
